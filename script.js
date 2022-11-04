4978d120e4fc2f6e32a171dafa527dfcbc22ac02
Dharmesh Borda <dreamzntrend@gmail.com>
2022-11-04 16:06:40 +0000

    channels completed

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ var swiper = new Swiper(".mySwiper", {
 
 var swiper2 = new Swiper(".mySwiper2", {
     slidesPerView: 2,
-    spaceBetween: 20,
+    spaceBetween: 15,
     pagination: {
         el: ".swiper-pagination",
         type: "progressbar",
@@ -57,6 +57,19 @@ var swiper2 = new Swiper(".mySwiper2", {
 var swiper3 = new Swiper(".mySwiper3", {
     slidesPerView: 7,
     spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper4 = new Swiper(".mySwiper4", {
+    slidesPerView: 9,
+    spaceBetween: 20,
     pagination: {
         el: ".swiper-pagination",
         type: "progressbar",
